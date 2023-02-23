2178be63bd51130f21e41ebdd4cd1c2eafd59ca3
Jared Doerr <jared.doerr@advancedmd.com>
2023-02-22 23:08:14 -0700

    Checking super cool grade evlauator with stretch goals.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        bool ableToParseGradeValue = false;
+        double parsedGradeValue = 0;
+        string enteredGradeValue = "";
+        while (!ableToParseGradeValue)
+        {
+            Console.Write("What is your numeric grade value ? : ");
+            enteredGradeValue = Console.ReadLine();
+
+            ableToParseGradeValue = double.TryParse(enteredGradeValue, out parsedGradeValue);
+            if (!ableToParseGradeValue)
+            {
+                Console.WriteLine("The value you entered is not a numeric value please try again.");
+            }
+        }
+        Console.WriteLine($"Your entered numeric grade value {enteredGradeValue.ToString()} is a {GetGradeLetterFromNumericValue(parsedGradeValue)}");
     }
+
+    static string GetPlusOrMinusInidicator(double numericGradeValue)
+    {
+        double onesRemainder = numericGradeValue % 10;
+        string plusOrMinusIndicator = "";
+        if (onesRemainder >= 7)
+        {
+            if (numericGradeValue < 90)
+            {
+                plusOrMinusIndicator = "+";
+            }
+        }
+
+        if (onesRemainder < 3)
+        {
+            plusOrMinusIndicator = "-";
+
+        }
+        return plusOrMinusIndicator;
+    }
+
+    static string GetGradeLetterFromNumericValue(double numericGradeValue)
+    {
+
+        if (numericGradeValue >= 90)
+        {
+            return "A" + GetPlusOrMinusInidicator(numericGradeValue);
+        }
+        if (numericGradeValue >= 80)
+        {
+            return "B" + GetPlusOrMinusInidicator(numericGradeValue);
+        }
+        if (numericGradeValue >= 70)
+        {
+            return "C" + GetPlusOrMinusInidicator(numericGradeValue);
+        }
+        if (numericGradeValue >= 60)
+        {
+            return "D" + GetPlusOrMinusInidicator(numericGradeValue);
+        }
+        return "F";
+    }
+
 }
