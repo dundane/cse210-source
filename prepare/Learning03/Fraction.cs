f7c7550f83dea29366de2263c2637f736fb6d13f
Jared Doerr <jared.doerr@advancedmd.com>
2023-03-08 19:06:13 -0700

    Checking in Fraction Stuff

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning03 {
+  public class Fraction {
+    private int topOfFraction = 1;
+    private int bottomOfFraction = 1;
+
+    public Fraction() { 
+      topOfFraction = 1;
+      bottomOfFraction = 1;
+    }
+    public Fraction(int wholeNumber) { 
+      topOfFraction= wholeNumber;
+      bottomOfFraction = 1;
+    }
+    public Fraction(int topNumber, int bottomNumber) {
+      this.topOfFraction = topNumber;
+      this.bottomOfFraction = bottomNumber;
+    }
+
+    public int GetTopNumber() {
+      return topOfFraction;
+    }
+
+    public int GetBottomNumber() {  
+      return bottomOfFraction;
+    }
+
+    public void SetTopNumber(int topNumber) {
+      topOfFraction = topNumber;
+    }
+
+    public void SetBottomNumber(int bottomNumber) { 
+      bottomOfFraction = bottomNumber;
+    }
+
+    public string GetFractionString() {
+      return $"{topOfFraction}/{bottomOfFraction}";
+    }
+
+    public double GetDecimalValue() { 
+      return Convert.ToDouble(topOfFraction)/Convert.ToDouble(bottomOfFraction);
+    }
+
+  }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,20 @@
+using Learning03;
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
+class Program {
+  static void Main(string[] args) {
+    Fraction constructorOne = new Fraction();
+    Fraction constructorTwo = new Fraction(5);
+    Fraction constructorThree = new Fraction(3, 4);
+    Fraction constructorFour = new Fraction();
+    constructorFour.SetTopNumber(1);
+    constructorFour.SetBottomNumber(3);
+    List<Fraction> fractions = new List<Fraction>() { constructorOne, constructorTwo, constructorThree, constructorFour };
+    int listCounter = 0;
+    foreach (Fraction fraction in fractions) {
+      listCounter++;
+      Console.WriteLine($"Constructor {listCounter} String: {fraction.GetFractionString()}");
+      Console.WriteLine($"Constructor {listCounter} Decimal: {fraction.GetDecimalValue()}");
     }
+  }
 }
